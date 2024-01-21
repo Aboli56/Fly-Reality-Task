4094c297988802c0d0fcca5c7340f460ab27ed6c
Aboli Wankhade <104322603+Aboli56@users.noreply.github.com>
2024-01-21 13:19:33 +0530

    Add files via upload

---
+++ src/Redux/gameTrack.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import gameReducerStore from "./reducers/gameReducerStore";
+
+const store = configureStore({
+  reducer: {
+    game: gameReducerStore, // Assuming "game" as the slice name for the game reducer
+  },
+});
+
+export default store;
